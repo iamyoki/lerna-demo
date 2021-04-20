d9b59b8f14e17277fbc8cee2770e0463c64df129
Yoki Yu <yokiyuqian@gmail.com>
2021-04-20 15:42:23 +0800

    feat: remove some code

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,4 +1,3 @@
 exports.add = function add(...args) {
-  console.log( 'add calc' )
   return args.reduce((total, cur) => total + cur, 0)
 }
--- packages/line/index.js
+++ packages/line/index.js
@@ -2,7 +2,7 @@ const { add } = require('@stui/core')
 
 // line is a lib
 function line(...args) {
-  return add(args, 'Very good')
+  return add(args)
 }
 
 console.log( line(1,2,3,4) )
